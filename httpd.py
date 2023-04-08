09873862aefb071fe37e2d43d95c47471fafeed1
Anton Balabanov <asbbnv@gmail.com>
2023-04-08 18:54:09 +0300

    basic server with selectors module

--- httpd.py
+++ httpd.py
@@ -3,14 +3,11 @@ import logging
 import select
 import selectors
 
-# import ssl
-
 HOST = 'localhost'
 PORT = 8081
 
 selector = selectors.DefaultSelector()
 
-to_monitor = []
 
 response ="""HTTP/1.0 200 OK
 Date: Thu, 26 Apr 2007 19:54:01 GMT
@@ -23,44 +20,50 @@ Content-Type: text/html
 </HTML>
 """
 
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                 1)  # socket option level, socket option, reuse address after close
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(5)
+    logging.info('Server started')
+    # server_file = server_socket.makefile('rw')
+    selector.register(fileobj=server_socket, events=selectors.EVENT_READ, data=accept_connections)
+
 
 def send_message(client_socket):
     request = client_socket.recv(1024)
-
+    # request = client_file.readline()
     if request:
         logging.info(f"Got request: {request.decode('utf-8')}")
         data = f"{response}\r\n".encode('utf-8')
         client_socket.send(data)
+        # client_file.write(data)
+        # client_file.flush()
         logging.info(f"Sent to {client_socket.getpeername()}, data: {data}")
     else:
+        selector.unregister(client_socket)
         client_socket.close()
 
 
 def accept_connections(server_socket):
     client_socket, a = server_socket.accept()
+    # client_file = client_socket.makefile('rw')
     logging.info(f'Received connection from {a}')
-    to_monitor.append(client_socket)
+    selector.register(fileobj=client_socket, events=selectors.EVENT_READ, data=send_message)
 
 
-def event_loop(server_socket):
+def event_loop():
     while 1:
-        ready_to_read, _, _ = select.select(to_monitor, [], [])
-        for sock in ready_to_read:
-            if sock is server_socket:
-                accept_connections(sock)
-            else:
-                send_message(sock)
+        events = selector.select()
+        for key, _ in events:
+            callback = key.data
+            callback(key.fileobj)
 
 
 if __name__ == "__main__":
     log_format: str = '%(asctime)s %(levelname).1s %(message)s'
     logging.basicConfig(format=log_format, filename='', level='INFO',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
-                 1)  # socket option level, socket option, reuse address after close
-    s.bind((HOST, PORT))
-    s.listen(5)
-    logging.info('Server started')
-    to_monitor.append(s)
-    event_loop(s)
+    server()
+    event_loop()
